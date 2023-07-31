63285f9bbde46f664749c044bf8667265f0eae64
cluster <cluster@modernintelligence.ai>
2023-07-30 19:16:58 -0700

    Temporary fix to using multiple processes by using csv files

--- train_optuna.py
+++ train_optuna.py
@@ -1,124 +1,169 @@
 import os
 import time
 import random
-import logging
 from pathlib import Path
 
-import wandb
+import pandas as pd 
+import os.path
+
+import wandb  
 import optuna
 from optuna.trial import Trial
 
 import torch.utils.data
-from torch.nn.parallel import DistributedDataParallel
 from lightning.fabric import Fabric, seed_everything
 
 import models
 import data_utils
-import train
+import train  
 import optimizers
 import loss
+import os
 from utils.opt import get_cfgs
 
 torch.set_float32_matmul_precision("medium")
+
+# wandb might cause an error without this.
 os.environ["WANDB_START_METHOD"] = "thread"
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def objective(trial: Trial, cfgs: dict, fabric: Fabric, rand_seed: int):
 
+    # suggest values of hyperparam using a trial object
+    hyperparams = {
+        # "lr": trial.suggest_float("lr", 8e-6, 1e-5, log=False),
+        "lr": trial.suggest_float("lr", 1e-5, 1e-5, log=False), # default
+        "weight_decay": trial.suggest_float("weight_decay", 0.001, 0.001, log=False),
+        "beta1": trial.suggest_float("beta1", 0.80, 0.85),
+        "beta2": trial.suggest_float("beta2", 0.97, 0.98)
+        # "warmup_steps": trial.suggest_int("warmup_steps", 100, 800),
+        # "cycles": trial.suggest_float("cycles", 0.1, 1.0),
+    }
 
-def objective(trial: Trial, cfgs: dict, fabric: Fabric, rand_seed: int) -> float:
-    """Objective function for Optuna hyperparameter optimization."""
-
-    # Removed hardcoding of hyperparameters
-    hyperparams = cfgs.hyperparameters
-    for hyperparam in hyperparams.keys():
-        hyperparams[hyperparam] = trial.suggest_float(hyperparam, *cfgs.hyperparameters_range[hyperparam])
-
+    # update cfgs with suggested hyperparameters
     for hyperparam, value in hyperparams.items():
         setattr(cfgs, hyperparam, value)
 
-    trial_name = cfgs.wandb_trial_name + "_trial_" + str(trial.number) if cfgs.wandb_trial_name else "trial_" + str(trial.number)
+    if cfgs.wandb_trial_name:
+        trial_name = cfgs.wandb_trial_name + "_trial_" + str(trial.number)
+    else:
+        trial_name = "trial_" + str(trial.number)
+
+    if fabric.is_global_zero:
+        wandb.init(
+            project=cfgs.wandb_project,
+            config=cfgs,
+            group=cfgs.study_name,
+            name=trial_name,
+            reinit=True,
+        )
+        wandb.run.save()
+
+        # log optuna hyperparams to wandb once
+        for hyperparam, value in hyperparams.items():
+            wandb.log({"hyperparams/"+str(hyperparam): value})
+        wandb.log({"hyperparams/seed": rand_seed})
+
+    # log trial number to cfgs
     cfgs.trial_number = trial.number
 
     highest_val = main(cfgs, fabric, trial)
 
     return highest_val
 
+def main(cfgs: dict, fabric, trial: Trial = None):
 
-def main(cfgs: dict, fabric: Fabric, trial: Trial = None) -> float:
-    """Main function for the training/evaluation process."""
-
+    # create the dataset and dataloader
     train_dataset, val_dataset, train_loader, val_loader = data_utils.get_dataset_and_dataloader(cfgs)
+
+    # Shape: batch_size, modalities, triplet, channels, height, width
     train_loader, val_loader = fabric.setup_dataloaders(train_loader, val_loader)
 
     dataset_size = len(train_dataset) + len(val_dataset)
-    logger.info("Dataset size total: {}".format(dataset_size))
 
+    if fabric.is_global_zero:
+        print("Dataset size total:", dataset_size)
+        print("Training set size:", len(train_dataset))
+        print("Validation set size:", len(val_dataset))
+
+    # create output directory
     config_name = os.path.splitext(os.path.basename(cfgs.cfg_name))[0]
     unique_dir_name = time.strftime("%Y%m%d-%H%M%S-") + config_name
+
     output_dir = Path(cfgs.output_dir, unique_dir_name)
     output_dir.mkdir(parents=True, exist_ok=True)
-    logger.info("Output directory: {}".format(output_dir))
 
+    print("Output directory:", output_dir)
+
+    # Get the model
     model = models.get_model(cfgs=cfgs, fabric=fabric)
-    model = DistributedDataParallel(model, device_ids=fabric.devices)  # Ensure model is ready for multi-GPU
 
     criterion = loss.get_loss(cfgs, fabric)
+
     optimizer = optimizers.get_optim(cfgs, model)
 
-    trainer = train.get_trainer(cfgs, fabric, model, train_loader, val_loader,
-                                optimizer, criterion, unique_dir_name, trial)
+    trainer = train.get_trainer(cfgs, fabric, model, train_loader, val_loader, optimizer, criterion, unique_dir_name, trial)
 
-    highest_val = trainer.train() if cfgs.phase == "train" else trainer.validate()  # Return the validation score
+    # print out model summary
+    if fabric.is_global_zero:
+        trainer.print_networks()
+
+    # begin training
+    if cfgs.phase == "train":
+        highest_val = trainer.train()
+    elif cfgs.phase == "val":
+        print(trainer.validate())
 
     return highest_val
 
-
 if __name__ == "__main__":
-
     cfgs = get_cfgs()
+    if cfgs.output_dir:
+        Path(cfgs.output_dir).mkdir(parents=True, exist_ok=True)
 
-    Path(cfgs.output_dir).mkdir(parents=True, exist_ok=True)
-
-    rand_seed = random.randint(0, 2**32 - 1) if cfgs.new_random else cfgs.seed
+    # Set random seed for reproduceability
+    if cfgs.new_random:
+        rand_seed = random.randint(0, 2**32 - 1)
     cfgs.seed = rand_seed
     seed_everything(cfgs.seed)
 
-    fabric = Fabric(accelerator="auto", devices=cfgs.gpus)
+    fabric = Fabric(accelerator="auto", devices = cfgs.gpus)
     fabric.launch()
 
-    sqlite_file = os.path.join(os.getcwd(), f'{cfgs.study_name}.db')
-    storage_name = f"sqlite:///{sqlite_file}"
+    # set up optuna study
+    df_path = 'trials.csv'
 
+    if os.path.exists(df_path):
+        df = pd.read_csv(df_path)
+        study.add_trials(df)
+    else:
+        df = None
+        
     sampler = optuna.samplers.QMCSampler(seed=cfgs.seed)
-    try:
-        study = optuna.load_study(study_name=cfgs.study_name, storage=storage_name)
-        logger.info("Study {} loaded.".format(cfgs.study_name))
-    except KeyError:
-        logger.info("Study {} not found, creating a new one.".format(cfgs.study_name))
-        study = optuna.create_study(
-            direction="maximize",
-            study_name=cfgs.study_name,
-            pruner=optuna.pruners.MedianPruner(n_startup_trials=1, n_warmup_steps=100),
-            sampler=sampler,
-            storage=storage_name,
-        )
+    # sampler = optuna.samplers.TPESampler(seed=cfgs.seed)
+    study = optuna.create_study(
+        direction="maximize", 
+        study_name=cfgs.study_name,
+        pruner=optuna.pruners.MedianPruner(
+            n_startup_trials=1,
+            n_warmup_steps=100
+        ),
+        sampler=sampler,
+    )
 
-    try:
-        study.optimize(lambda trial: objective(trial, cfgs, fabric, cfgs.seed),
-                       n_trials=1,
-                       n_jobs=cfgs.n_jobs)  # Parallelize trials across configurable number of processes
-    except Exception as e:
-        logger.error("Error: {}".format(e))
+    study.optimize(
+        lambda trial: objective(trial, cfgs, fabric, cfgs.seed),
+        n_trials=1,
+        n_jobs=1,
+    )
+    
+    if df is None:
+        df = study.trials_dataframe()
+        df.to_csv(df_path)
 
-    logger.info("Best trial:")
+    # report results to console
+    print("Best trial:")
     trial = study.best_trial
-    logger.info("  Value: {}".format(trial.value))
-    logger.info("  Params: ")
+    print("  Value: ", trial.value)
+    print("  Params: ")
     for key, value in trial.params.items():
-        logger.info("    {}: {}".format(key, value))
-
-    # Cleanup SQLite database file
-    if os.path.exists(sqlite_file):
-        os.remove(sqlite_file)
+        print("    {}: {}".format(key, value))
