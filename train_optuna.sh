36414343d7f3b549689793dd6f7f6cecd5de2f96
cluster <cluster@modernintelligence.ai>
2023-08-28 10:38:19 -0700

    Finish modality ablations stage 1

--- train_optuna.sh
+++ train_optuna.sh
@@ -4,5 +4,6 @@
 SEED=$(date +%N)
 
 # Run the Python script with the --seed flag set to the random number
-python train_optuna.py -c cfgs/deit_gradual_fusion_rnt100_stage1.yaml # --seed $SEED
+# python train_optuna.py -c cfgs/deit_gradual_fusion_rnt100_stage1.yaml # --seed $SEED
 # python train_optuna.py -c cfgs/deit_gradual_fusion_rn300_stage1.yaml
+python train_optuna.py -c cfgs/deit_gradual_fusion_rnt100_stage1_modality_ablation.yaml
