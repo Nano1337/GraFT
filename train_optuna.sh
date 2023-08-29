0ded1f0fad803d3152d24b3a28d78b7395959d65
cluster <cluster@modernintelligence.ai>
2023-08-29 08:36:15 -0700

    Begin running stage2 random step ablations

--- train_optuna.sh
+++ train_optuna.sh
@@ -6,4 +6,7 @@ SEED=$(date +%N)
 # Run the Python script with the --seed flag set to the random number
 # python train_optuna.py -c cfgs/ablations/deit_vanilla_fusion_rnt100_stage1.yaml # --seed $SEED
 
-python train_optuna.py -c cfgs/deit_gradual_fusion_rnt100_stage2_modality_ablation.yaml
+export WANDB_API_KEY=b730655c05d6f0514256a0372293a335e8a5aa1a
+export WANDB_DIR=/home/ubuntu/haoli3/research-GraFT/wandb
+
+python train_optuna.py -c cfgs/ablations/deit_gradual_fusion_rnt100_stage2_random_step_ablation.yaml
